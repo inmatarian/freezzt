74794dd94337dac629f3381c12f12a73bd21e633
Inny <inmatarian@gmail.com>
2009-11-09 18:31:46 -0500

    Switch to a more portable friendly way of file loading Eliminate namespacing, that was dumb Eliminate ZZT::String

--- src/worldLoader.cpp
+++ src/worldLoader.cpp
@@ -16,6 +16,90 @@
 #include "zztEntity.h"
 #include "zztStructs.h"
 
+// ---------------------------------------------------------------------------
+// utility functions, define as needed for platform endianness
+
+// zzt binary to little endian
+static unsigned char zztByte( const unsigned char *data )
+{
+  return *data;
+}
+
+// zzt binary to little endian
+static signed short zztWord( const unsigned char *data )
+{
+  return *(reinterpret_cast<const signed short*>(data));
+}
+
+// zzt binary to little endian
+static std::string zztString( const unsigned char *data )
+{
+  int len = *data;
+  if (len > 127) len = 127; // minor security detail
+  return std::string( (const char *)(data + 0x01), len );
+}
+
+// ---------------------------------------------------------------------------
+// taken mostly from kev's file format
+
+WorldHeader::WorldHeader( const unsigned char *data )
+{
+  magicKey = (unsigned short) zztWord( data + 0x00 );
+  boardCount = zztWord( data + 0x02 );
+  ammo = zztWord( data + 0x04 );
+  gems = zztWord( data + 0x06 );
+
+  blueKey = zztByte( data + 0x08 );
+  greenKey = zztByte( data + 0x09 );
+  cyanKey = zztByte( data + 0x0A );
+  redKey = zztByte( data + 0x0B );
+  purpleKey = zztByte( data + 0x0C );
+  yellowKey = zztByte( data + 0x0D );
+  whiteKey = zztByte( data + 0x0E );
+
+  health = zztWord( data + 0x0F );
+  startBoard = zztWord( data + 0x11 );
+  torches = zztWord( data + 0x13 );
+  torchCycles = zztWord( data + 0x15 );
+  energizerCycles = zztWord( data + 0x17 );
+  score = zztWord( data + 0x1B );
+
+  gameName = zztString( data + 0x1D );
+
+  for ( int x = 0; x < 10; x++ ) {
+    flags[x] = zztString( data + (x * 21) + 0x32 );
+  }
+
+  time = zztWord( data + 0x104 );
+  savegame = zztByte( data + 0x108 );
+}
+
+BoardHeader::BoardHeader( const unsigned char *data )
+{
+  sizeInBytes = zztWord( data );
+  title = zztString( data + 0x02 );
+}
+
+BoardInformation::BoardInformation( const unsigned char *data )
+{
+  maximumShotsFired = zztByte( data + 0x00 );
+  darkness = zztByte( data + 0x01 );
+  boardNorth = zztByte( data + 0x02 );
+  boardSouth = zztByte( data + 0x03 );
+  boardWest = zztByte( data + 0x04 );
+  boardEast = zztByte( data + 0x05 );
+  reenterZapped = zztByte( data + 0x06 );
+
+  message = zztString( data + 0x07 );
+
+  enterX = zztByte( data + 0x42 );
+  enterY = zztByte( data + 0x43 );
+  timeLimit = zztWord( data + 0x44 );
+  thingCount = zztWord( data + 0x56 );
+}
+
+// ---------------------------------------------------------------------------
+
 class WorldLoaderPrivate
 {
   public:
@@ -46,15 +130,15 @@ GameBoard * WorldLoaderPrivate::loadBoard( int &filePos )
   board->setWorld( world );
   board->clear();
 
-  ZZT::BoardHeader *header = new(worldData + filePos) ZZT::BoardHeader;
+  std::auto_ptr<BoardHeader>
+      header( new BoardHeader(worldData + filePos) );
 
   const int origFilePos = filePos;
   const int endFilePos = filePos + header->sizeInBytes + 2;
 
   zinfo() << "Board header: "
           << header->sizeInBytes << " "
-          << sizeof( ZZT::BoardHeader ) << " "
-          << header->title.toStdString();
+          << header->title;
 
   filePos += 0x35;
   zdebug() << "Filepos: " << std::hex << filePos << std::dec;
@@ -65,13 +149,14 @@ GameBoard * WorldLoaderPrivate::loadBoard( int &filePos )
     return 0;
   }
 
-  ZZT::BoardInformation *info = new(worldData + filePos) ZZT::BoardInformation;
+  std::auto_ptr<BoardInformation>
+      info( new BoardInformation(worldData + filePos) );
 
   zinfo() << "Board info: "
           << info->thingCount << " "
-          << info->message.toStdString();
+          << info->message;
 
-  board->setMessage( info->message.toStdString() );
+  board->setMessage( info->message );
   board->setNorthExit( info->boardNorth );
   board->setSouthExit( info->boardSouth );
   board->setWestExit( info->boardWest );
@@ -157,7 +242,8 @@ bool WorldLoader::go()
 
   int filePos = 0;
 
-  ZZT::WorldHeader *header = new(d->worldData) ZZT::WorldHeader;
+  std::auto_ptr<WorldHeader>
+      header( new WorldHeader(d->worldData) );
 
   zinfo() << "Magickey: " << header->magicKey;
   if ( header->magicKey != 0xffff ) {
@@ -165,7 +251,7 @@ bool WorldLoader::go()
     return false;
   }
  
-  d->world->setWorldTitle( header->gameName.toStdString() );
+  d->world->setWorldTitle( header->gameName );
   zinfo() << "GameName: " << d->world->worldTitle();
 
   // load counters
@@ -208,7 +294,7 @@ bool WorldLoader::go()
 
   // load gameflags
   for ( int x = 0; x < 10; x++ ) {
-    std::string str = header->flags[x].toStdString();
+    std::string str = header->flags[x];
     if ( !str.empty() ) {
       d->world->addGameFlag( str );
       zinfo() << "Flag: " << str;
@@ -217,7 +303,7 @@ bool WorldLoader::go()
 
   int boards = header->boardCount + 1;
   zinfo() << "Adding boards: " << boards;
-  filePos += sizeof( ZZT::WorldHeader );
+  filePos += 0x200;
 
   for ( int x = 0; x < boards; x++ ) {
     GameBoard *board = d->loadBoard(filePos);
--- src/zztStructs.h
+++ src/zztStructs.h
@@ -3,37 +3,11 @@
 #ifndef ZZT_STRUCTS_H
 #define ZZT_STRUCTS_H
 
-// ---------------------------------------------------------------------------
-// kev's file format
-
-namespace ZZT {
-
-template <int t>
-struct String
-{
-  unsigned char length;
-  char name[t];
-
-  std::string toStdString() const
-  {
-    int len = ( length <= t
-                ? length
-                : 0 );
-    char str[t+1];
-
-    for ( int x = 0; x < len; x++ ) {
-      str[x] = name[x];
-    }
-    str[len] = 0;
-
-    return std::string(str);    
-  };
-}
-__attribute__((__packed__));
-
 struct WorldHeader
 {
-  unsigned short magicKey;   // FFFFh
+  WorldHeader( const unsigned char *data );
+
+  unsigned short magicKey; // FFFFh
   signed short boardCount; // minus 1, so 0x0 is 1, 0x1 is 2, etc.
   signed short ammo;
   signed short gems;
@@ -49,27 +23,25 @@ struct WorldHeader
   signed short torches;
   signed short torchCycles;
   signed short energizerCycles;
-  unsigned short blank_first;
   signed short score;
-  ZZT::String<20> gameName;
-  ZZT::String<20> flags[10];
+  std::string gameName;
+  std::string flags[10];
   unsigned short time;
-  unsigned short blank_second;
   unsigned char savegame;
-  unsigned char blank_third[247];
-}
-__attribute__((__packed__));
+};
 
 struct BoardHeader
 {
+  BoardHeader( const unsigned char *data );
+
   signed short sizeInBytes;
-  ZZT::String<33> title;
-  unsigned char blank[16];
-}
-__attribute__((__packed__));
+  std::string title;
+};
 
 struct BoardInformation
 {
+  BoardInformation( const unsigned char *data );
+
   unsigned char maximumShotsFired;
   unsigned char darkness;
   unsigned char boardNorth;
@@ -77,16 +49,12 @@ struct BoardInformation
   unsigned char boardWest;
   unsigned char boardEast;
   unsigned char reenterZapped;
-  ZZT::String<58> message;
+  std::string message;
   unsigned char enterX;
   unsigned char enterY;
   signed short timeLimit;
-  unsigned char padding[16];
   signed short thingCount;
-}
-__attribute__((__packed__));
-
-}; // namespace ZZT
+};
 
 #endif // ZZT_STRUCTS_H
 
